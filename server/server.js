aabae618d23499689d92249bf0697f6b55a0a129
tatsusei <doubledragontaz@gmail.com>
2020-04-13 20:13:15 +0900

    create list API

--- server/server.js
+++ server/server.js
@@ -5,17 +5,44 @@ const fs = require('fs')
 
 let aboutMessage="Issue Tracker API v1.0"
 
+const issuesDB = [
+    {
+        id: 1, 
+        status: 'New', 
+        owner: 'Ravan', 
+        effort: 5, 
+        created: new Date('2019-01-15'), 
+        due: undefined, 
+        title: 'Error in console when clicking Add',
+    }, 
+    {
+        id: 2, 
+        status: 'Assigned', 
+        owner: 'Eddie', 
+        effort: 14, 
+        created: new Date('2019-01-16'), 
+        due: new Date('2019-02-01'), 
+        title: 'Missing bottom border on panel',
+    },
+];
+
 const resolvers = {
     Query:{
         about:() => aboutMessage,
+        issueList,
     },
     Mutation: {
         setAboutMessage,
     },
 };
 
+
 function setAboutMessage(_, { message }) { 
     return aboutMessage = message;
+}
+
+function issueList() {
+    return issuesDB;
 }
 
 

6c13c97d253c6a3c32c799aeb5e2f702a2111868
tatsusei <doubledragontaz@gmail.com>
2020-04-16 22:30:22 +0900

    Libraries Bundle

---
+++ ui/src/IssueFilter.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue filter.</div>
+        );
+    }
+};

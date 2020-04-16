6655d19ecf21adf8141fde97758a251bf6dcc5db
tatsusei <doubledragontaz@gmail.com>
2020-04-16 22:52:11 +0900

    Hot Module Replacement

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -5,6 +5,23 @@ const apiProxyTarget = process.env.API_PROXY_TARGET;
 
 const app = express();
 
+const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
+if (enableHMR && (process.env.NODE_ENV !== 'production')) { 
+    console.log('Adding dev middleware, enabling HMR');
+    /* eslint "global-require": "off" */
+    /* eslint "import/no-extraneous-dependencies": "off" */ 
+    const webpack = require('webpack');
+    const devMiddleware = require('webpack-dev-middleware'); 
+    const hotMiddleware = require('webpack-hot-middleware');
+    const config = require('./webpack.config.js'); 
+    config.entry.app.push('webpack-hot-middleware/client'); 
+    config.plugins = config.plugins || [];
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+    const compiler = webpack(config); 
+    app.use(devMiddleware(compiler)); 
+    app.use(hotMiddleware(compiler));
+}
+
 app.use(express.static('public'));
 if (apiProxyTarget ){
     app.use('/graphql',proxy({target: apiProxyTarget}))

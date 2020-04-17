266391efcae34589c43286652cb00d8bf39e7ff6
tatsusei <doubledragontaz@gmail.com>
2020-04-17 15:08:28 +0900

    Programmatic Navigation

--- ui/src/IssueFilter.jsx
+++ ui/src/IssueFilter.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-export default class IssueFilter extends React.Component {
+
+class IssueFilter extends React.Component {
+
+    constructor() {
+        super();
+        this.onChangeStatus = this.onChangeStatus.bind(this);
+    }
+
+    onChangeStatus(e) {
+        const status = e.target.value;
+        const {history} = this.props;
+        history.push({
+            pathname: '/issues',
+            search: status ? `?status=${status}` : ''
+        })
+    }
+
     render() {
         return (
             <div>
-                <Link to="/issues">All Issue</Link>
-                { ' | ' }
-                <Link to={{ pathname: '/issues', search: '?status=New' }}>New Issue</Link>
-                { ' | ' }
-                <Link to={{ pathname: '/issues', search: '?status=Assigned' }}>Assigned Issues</Link>
+                Status:
+                {' '}
+                <select  onChange={ this.onChangeStatus }>
+                    <option value="">(ALL)</option>
+                    <option value="New">New</option>
+                    <option value="Assigned">Assigned</option>
+                    <option value="Fixed">Fixed</option>
+                    <option value="Closed">Closed</option>
+                </select>
             </div>
+            // <div>
+            //     <Link to="/issues">All Issue</Link>
+            //     { ' | ' }
+            //     <Link to={{ pathname: '/issues', search: '?status=New' }}>New Issue</Link>
+            //     { ' | ' }
+            //     <Link to={{ pathname: '/issues', search: '?status=Assigned' }}>Assigned Issues</Link>
+            // </div>
         );
     }
 };
+
+export default withRouter(IssueFilter)

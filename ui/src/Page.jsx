23bdbbadb17d52d575d3a64e243ad0da4151ab0a
tatsusei <doubledragontaz@gmail.com>
2020-04-18 17:48:41 +0900

    Format file

--- ui/src/Page.jsx
+++ ui/src/Page.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import Contents from './Contents.jsx'
-import { NavLink } from 'react-router-dom'
+import Contents from "./Contents.jsx";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-    return(
-        <nav>
-            <NavLink exact to="/">Home</NavLink>
-            {' | '}
-            <NavLink to="/issues">Issue List</NavLink>
-            {' | '}
-            <NavLink to="/report">Report</NavLink>
-        </nav>
-    )
+  return (
+    <nav>
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink to="/issues">Issue List</NavLink>
+      {" | "}
+      <NavLink to="/report">Report</NavLink>
+    </nav>
+  );
 }
 
 export default function Page() {
-    return(
-        <div>
-            <Navbar></Navbar>
-            <Contents></Contents>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Contents></Contents>
+    </div>
+  );
 }

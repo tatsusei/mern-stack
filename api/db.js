afdbb7bf2a1988f0d82a630e244644cd13823b25
tatsusei <doubledragontaz@gmail.com>
2020-04-25 23:11:56 +0900

    Format file

--- api/db.js
+++ api/db.js
@@ -1,25 +1,30 @@
-require('dotenv').config()
-const { MongoClient } = require('mongodb')
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
 async function connectToDb() {
-    const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
-    const client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true })
-    await client.connect();
-    console.log('Connected to MongoDB at', url )
-    db = client.db()
+  const url = process.env.DB_URL || "mongodb://localhost/issuetracker";
+  const client = new MongoClient(url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  await client.connect();
+  console.log("Connected to MongoDB at", url);
+  db = client.db();
 }
 
 async function getNextSequence(name) {
-    const result = await db.collection('counters').findOneAndUpdate(
-        { _id: name},
-        { $inc:  { current:1 }},
-        { returnOriginal: false},
-    )
-    return result.value.current
+  const result = await db
+    .collection("counters")
+    .findOneAndUpdate(
+      { _id: name },
+      { $inc: { current: 1 } },
+      { returnOriginal: false }
+    );
+  return result.value.current;
 }
 
-function getDb(){
-    return db;
+function getDb() {
+  return db;
 }
 
-module.exports = { connectToDb, getNextSequence, getDb }
+module.exports = { connectToDb, getNextSequence, getDb };
